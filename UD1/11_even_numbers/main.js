aa9701101872d943dbd5910e192db1c5299f8fe3
Luis Mayayo <a25597@svalero.com>
2024-10-07 16:50:02 +0200

    ejercicio 11 y ejercicio 12 UNIDAD 1

--- UD1/11_even_numbers/main.js
+++ UD1/11_even_numbers/main.js
@@ -1,6 +1,14 @@
-function evenNumbers() {
-    
+function mostrarNumerosPares() {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 2 === 0) {
+            console.log(i);
+        }
+    }
 }
+
+// Llamamos a la función para ejecutar el código
+mostrarNumerosPares();
+
 
 evenNumbers();
 // expected output 2,4,6,8,10......98
