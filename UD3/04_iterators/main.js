09c0c3b90b8e74c7ff512c51b7b163f4e9cef932
Luis Mayayo <a25597@svalero.com>
2024-10-21 17:10:50 +0200

    unidad 3 - ejercicio 4(iterators)

--- UD3/04_iterators/main.js
+++ UD3/04_iterators/main.js
@@ -30,29 +30,60 @@ var pilots = [
 ];
 
 function iterateSimple() {
+    for (let pilot of pilots) {
+        console.log(pilot);
+    }
 }
+
 function iterateForEach() {
+    pilots.forEach(function (pilot) {
+        console.log(pilot);
+    });
 }
+
 function mapIds() {
+    // let ids = []
+
+    // for (let pilot of pilots) {
+    //     console.log(pilot);
+    //     ids.push(pilot.id);
+    // }
+
+    // return ids
+
+    return pilots.map((value) => value.id);
 }
+
 function rebels() {
+  return pilots
+            .filter((value) => value.faction === "Rebels")
+            // .map((value) =>value.id)
+            // .filter((value) => value > 5)
 }
+
 function totalFaction(faction) {
+    return pilots
+    .filter((value) => value.faction === faction)
+    .length
 }
+
 function avgYears(faction) {
+    let selectedFaction = pilots
+    .filter((value) => value.faction === faction)
+
+    return selectedFaction.reduce((pv, cv) => pv + cv.years, 0) / selectedFaction.length
 }
 
-// use console.log
-iterateSimple()
-iterateForEach()
+iterateSimple();
+iterateForEach();
+
 try {
-  assert.deepStrictEqual(mapIds(), [2,8,40,66])
+  assert.deepStrictEqual(mapIds(), [2, 8, 40, 66])
   assert.deepStrictEqual(rebels(), [pilots[0], pilots[3]])
-
   assert.deepStrictEqual(totalFaction('Rebels'), 2)
-
   assert.deepStrictEqual(avgYears('Rebels'), 22.5)
   assert.deepStrictEqual(avgYears('Empire'), 25)
+  console.log("All tests passed!");
 } catch (error) {
-  console.error(error)
+  console.error(error);
 }
