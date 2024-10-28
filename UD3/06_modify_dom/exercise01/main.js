e6de307c673b608c787c03fe12fb3dd722e4f33d
Luis Mayayo <a25597@svalero.com>
2024-10-28 17:01:52 +0100

    u3 ejercicio6.1

--- UD3/06_modify_dom/exercise01/main.js
+++ UD3/06_modify_dom/exercise01/main.js
@@ -1,5 +1,31 @@
 window.onload = function(e) {
-    console.log('documento cargado')
+    console.log('documento cargado');
 
-    // document.getElementsByTagName('h1')[0].innerText = 'Changed from JS!!'
+    //Cambiar el título del segundo h2
+    document.getElementsByTagName('h2')[1].innerText = 'Changed from JS!!';
+
+    //Cambiar el color de todos los .first que estén dentro de un artículo
+    const elements = document.querySelectorAll('article .first');
+    
+    elements.forEach(function(element) {
+        element.style.color = 'red';
+    });
+
+    //Seleccionar el elemento con id == username
+    document.getElementById('usename');
+
+    //Seleccionar todos lo elementos li con class == item
+    const items = document.querySelectorAll('li.item');
+    
+    //Seleccionar todos lo buttons dentro de class == buttons
+    const buttons = document.querySelectorAll('.buttons button');
+
+    //Cambiar el color de fondo del primer párrafo
+    document.querySelector('p')[0].style.backgroundColor = 'yellow';
+
+    //Cambiar el color de frente (forecolor) de los elementos buttons dentro de class == buttons
+    document.querySelectorAll('.buttons button').forEach(function(button) {
+        button.style.color = 'blue';
+    });
+
 }
