7cc27ff94c62e53e89ea7337ac6b70066a6d993d
Luis Mayayo <a25597@svalero.com>
2024-10-30 19:56:33 +0100

    ejercicio 7

--- UD3/07_events/main.js
+++ UD3/07_events/main.js
@@ -1,24 +1,20 @@
 window.onload = function() {
     console.log('documento cargado');
 
-    // Obtener todos los enlaces
     const enlaces = document.querySelectorAll('a[id^="enlace_"]');
     
-    // Iterar sobre cada enlace
     enlaces.forEach((enlace, index) => {
         enlace.addEventListener('click', function(e) {
-            e.preventDefault(); // Prevenir el comportamiento por defecto del enlace
+            e.preventDefault();
             
-            // Obtener la sección de contenido correspondiente
             const contenido = document.getElementById(`contenidos_${index + 1}`);
             
-            // Alternar la visibilidad de la sección
             if (contenido.style.display === 'none') {
-                contenido.style.display = 'block'; // Mostrar contenido
-                enlace.innerHTML = 'Ocultar contenidos'; // Cambiar texto
+                contenido.style.display = 'block';
+                enlace.innerHTML = 'Ocultar contenidos';
             } else {
-                contenido.style.display = 'none'; // Ocultar contenido
-                enlace.innerHTML = 'Mostrar contenidos'; // Cambiar texto
+                contenido.style.display = 'none';
+                enlace.innerHTML = 'Mostrar contenidos';
             }
         });
     });
