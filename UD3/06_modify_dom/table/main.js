56b30e45b76268444da2b4aebe3ce8fa52906692
Luis Mayayo <a25597@svalero.com>
2024-11-04 17:02:08 +0100

    u6_table

--- UD3/06_modify_dom/table/main.js
+++ UD3/06_modify_dom/table/main.js
@@ -1,19 +1,31 @@
+let currentId = 1;
+
 function onClick() {
-    let tbody = document.getElementsByTagName('tbody')[0]
+    let tbody = document.getElementsByTagName('tbody')[0];
 
-    let tr = document.createElement('tr')
-    let td = document.createElement('td')
-    td.innerText = '0'
-    tr.appendChild(td)
+    let tr = document.createElement('tr');
 
-    td = document.createElement('td')
-    td.innerText = 'Example'
-    tr.appendChild(td)
+    let td = document.createElement('td');
+    td.innerText = currentId;
+    tr.appendChild(td);
 
-    td = document.createElement('td')
-    td.innerText = 'Surname'
-    tr.appendChild(td)
+    td = document.createElement('td');
+    td.innerText = 'Example';
+    tr.appendChild(td);
 
-    tbody.appendChild(tr)
-    console.log('add')
+    td = document.createElement('td');
+    td.innerText = 'Surname';
+    tr.appendChild(td);
+
+    td = document.createElement('td');
+    let editLink = document.createElement('a');
+    editLink.href = `http://localhost/edit/${currentId}`;
+    editLink.innerText = 'Edit';
+    td.appendChild(editLink);
+    tr.appendChild(td);
+
+    tbody.appendChild(tr);
+    
+    currentId++;
+    console.log('add');
 }
